52c7c53a9d68362470e2b41fb0f95843b6cb9ad7
Vinicius Freitas <vinicius.freitas@sinch.com>
2022-04-19 13:50:50 -0300

    ek

--- TibiaRefil.js
+++ TibiaRefil.js
@@ -2,8 +2,11 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on("message", msg => {
-  if (msg.content === "refil.rp.gt") {
-    msg.reply("600x Spectral Bolt \n 300x Diamond Arrow \n 700x Great Mana Potion \n 400x Avalanche \n 250x SD");
+  if (msg.content === "rp.gt") {
+    msg.reply(" 600x Spectral Bolt \n 300x Diamond Arrow \n 700x Great Mana Potion \n 400x Avalanche \n 250x SD \n 2x Energy Ring");
+  }
+  if (msg.content === "ek.gt") {
+    msg.reply(" Arma (Fisico, Terra) \n 150x Supreme Health Potion \n 1500x Strong Mana Potion \n Plasma Ring e Prismatic Ring");
   }
 })
 
