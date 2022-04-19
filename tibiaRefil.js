35c544e42fc4a4d1039fab496848f7e60068314a
Vinicius Freitas <vinicius.freitas@sinch.com>
2022-04-19 14:58:03 -0300

    json ek

--- tibiaRefil.js
+++ tibiaRefil.js
@@ -2,6 +2,7 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const prefix = "!.";
 const infoRp = require("./lib/royalPaladin.json")
+const infoEk = require("./lib/eliteKnight.json")
 
 client.on("message", msg => {
   if (msg.content.includes(prefix)) {
@@ -14,8 +15,13 @@ client.on("message", msg => {
       msg.reply(infoRp.rotacao.refil);
     }
 
-    if (msg.content === "ek.gt") {
-      msg.reply(" Arma (Fisico, Terra) \n 150x Supreme Health Potion \n 1500x Strong Mana Potion \n Plasma Ring e Prismatic Ring");
+    if (msg.content === prefix + "ek.gt") {
+      msg.reply(infoEk.gt);
+    }
+
+    if (msg.content === prefix + "ek.rotacao") {
+      msg.reply(infoRp.rotacao.info);
+      msg.reply(infoRp.rotacao.refil);
     }
   }
 })
